c2150373f43c8202f7e1c282bc104ab1f1a6eca9
Marko Markovic <markomarkoviccb@gmail.com>
2024-09-28 15:00:46 +0200

    update header

--- truth-or-dare/src/components/Header.js
+++ truth-or-dare/src/components/Header.js
@@ -5,15 +5,10 @@ import '../styles/Header.css';
 const Header = () => {
     return (
         <header className="header-container">
-            <div>
+            <div className='header-content'>
                 <div className="logo">Truth or Dare</div>
-                <div className="description">Najluđi izazovi za tvoje društvo!</div>
+                <div className="description">Spremni za izazov?</div>
             </div>
-            <nav className="nav">
-                <Link className="nav-link" to="/">Početna</Link>
-                <Link className="nav-link" to="/play">Igraj odmah</Link>
-                <Link className="nav-link" to="/rules">Pravila igre</Link>
-            </nav>
         </header>
     );
 };

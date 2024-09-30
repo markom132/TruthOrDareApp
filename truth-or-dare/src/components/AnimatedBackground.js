98e615ae611920715925f02a1e00cdb61a2165de
Marko Markovic <markomarkoviccb@gmail.com>
2024-09-30 18:56:37 +0200

    add aditional animations

--- truth-or-dare/src/components/AnimatedBackground.js
+++ truth-or-dare/src/components/AnimatedBackground.js
@@ -2,21 +2,24 @@ import React from 'react';
 import '../styles/AnimatedBackground.css';
 
 const AnimatedBackground = () => {
-  return (
-    <div>
-    <div className="line left-one"></div>
-    <div className="line left-two"></div>
+    return (
+        <div>
+            <div className="line left-one"></div>
+            <div className="line left-two"></div>
 
-    <div className="line right-one"></div>
-    <div className="line right-two"></div>
+            <div className="line right-one"></div>
+            <div className="line right-two"></div>
 
-    <div className="circle left-one"></div>
-    <div className="circle left-two"></div>
+            <div className="circle left-one"></div>
+            <div className="circle left-two"></div>
 
-    <div className="circle right-one"></div>
-    <div className="circle right-two"></div>
-  </div>
-  );
+            <div className="circle right-one"></div>
+            <div className="circle right-two"></div>
+
+            <div className="glowing-square one"></div>
+            <div className="glowing-square two"></div>
+        </div>
+    );
 };
 
 export default AnimatedBackground;

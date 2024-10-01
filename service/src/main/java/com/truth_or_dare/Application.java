514fcc6623091cb6b2e17fcf5c77f1748c11a067
Marko Markovic <markomarkoviccb@gmail.com>
2024-10-01 13:44:04 +0200

    adding backend initial commit

---
+++ service/src/main/java/com/truth_or_dare/Application.java
@@ -0,0 +1,13 @@
+package com.truth_or_dare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ service/src/test/java/com/truth_or_dare/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truth_or_dare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

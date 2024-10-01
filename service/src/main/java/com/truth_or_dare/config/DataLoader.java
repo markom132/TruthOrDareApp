8ebd8c28f78433e1f65c16ec8b4c604e1740f862
Marko Markovic <markomarkoviccb@gmail.com>
2024-10-01 17:33:21 +0200

    update + fixes for APIs

--- service/src/main/java/com/truth_or_dare/config/DataLoader.java
+++ service/src/main/java/com/truth_or_dare/config/DataLoader.java
@@ -1,6 +1,10 @@
 package com.truth_or_dare.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.truth_or_dare.model.Dare;
+import com.truth_or_dare.model.Truth;
+import com.truth_or_dare.service.DareService;
+import com.truth_or_dare.service.TruthService;
 import jakarta.annotation.PostConstruct;
 import com.fasterxml.jackson.core.type.TypeReference;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,10 +21,16 @@ import java.util.Map;
 @Configuration
 public class DataLoader {
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private final TruthService truthService;
+    private final DareService dareService;
 
     private final String jsonFilePath = "data.json";
+
+    @Autowired
+    public DataLoader(TruthService truthService, DareService dareService) {
+        this.truthService = truthService;
+        this.dareService = dareService;
+    }
 
     @PostConstruct
     public void loadData() {
@@ -33,7 +43,15 @@ public class DataLoader {
             });
 
             for (Map<String, Object> entry : data) {
+                String type = (String) entry.get("type");
 
+                if (type.equalsIgnoreCase("truth")) {
+                    String question = (String) entry.get("question");
+                    truthService.saveTruth(Truth.builder().question(question).build());
+                } else if (type.equalsIgnoreCase("dare")) {
+                    String challenge = (String) entry.get("challenge");
+                    dareService.saveDare(Dare.builder().challenge(challenge).build());
+                }
             }
 
             System.out.println("Data successfully loaded into the H2 database.");
--- service/src/main/java/com/truth_or_dare/controller/TruthDareController.java
+++ service/src/main/java/com/truth_or_dare/controller/TruthDareController.java
@@ -8,10 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/api")
 public class TruthDareController {
 
